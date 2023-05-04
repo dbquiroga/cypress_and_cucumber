e723621de0cb5c232c39e44f0e99a0b9aed2b321
dbquiroga <day@xcapit.com>
2023-05-04 18:59:41 -0300

    adding git ignore

--- cypress/e2e/features/add_to_cart.feature
+++ cypress/e2e/features/add_to_cart.feature
@@ -2,11 +2,20 @@ Feature: Add a product to cart
 
     Scenario: User registration
         Given A user opens the demo blaze page
-        When A user enter username "Day"
+        When A user enter username
         When A user enter the "Qwerty123"
         When A user clicks in register button
+        Then The registration modal should not be visible
     
     Scenario: Login y logout user
+        Given A user should click on login btn
+        Then Opens the modal of Login
+        When A user enter the username as register
+        When A user enter the login password "Qwerty123"
+        When A user clicks in log in button
+        Then The login modal should not be visible
+        Then the navbar should have the name of the user
+
 
     Scenario: Agregar una laptop al carrito
     //Comprobar que se agregó al carrito.
--- cypress/e2e/step_definitions/add_to_cart.js
+++ cypress/e2e/step_definitions/add_to_cart.js
@@ -1,6 +1,11 @@
 import { When, Then, Given, Before, And } from "@badeball/cypress-cucumber-preprocessor";
 /// <reference types="cypress" />
 
+let datosFixture;
+let numero = Math.floor(Math.random() * 1000);
+let user = 'Day';
+let username = user + numero;
+
   Before(() =>{
     cy.fixture('demo_blaze').then((datos) =>{
         datosFixture = datos
@@ -12,16 +17,13 @@ import { When, Then, Given, Before, And } from "@badeball/cypress-cucumber-prepr
     cy.visit(baseUrl);
   });
 
-  When('A user enter username {string}', (user) =>{  
-    const numero = Math.floor(Math.random() * 1000);
+  When('A user enter username', (user) =>{  
     cy.get('#signin2').click();
 
     cy.get('#signInModal')
     .should('have.class', 'modal')
     .and('have.class', 'fade')
     .and('have.class', 'show');
-
-    const username = user + numero
 
     cy.get('#sign-username').clear();
     cy.get('#sign-username').type(username).trigger('input');
@@ -34,5 +36,54 @@ import { When, Then, Given, Before, And } from "@badeball/cypress-cucumber-prepr
   When('A user clicks in register button', () =>{
     cy.get('[type="button"]').contains('Sign up').dblclick();
   });
+
+  Then('The registration modal should not be visible',()=>{
+    cy.get('#signInModal')
+    .should('have.class', 'modal')
+    .and('have.class', 'fade')
+    .and('not.have.class', 'show');
+  });
   
+  Given('A user should click on login btn', () => {
+    cy.get('#login2').click() 
+  });
+
+  Then('Opens the modal of Login',() => {
+    cy.get('#logInModal')
+    .should('have.class', 'modal')
+    .and('have.class', 'fade')
+    .and('have.class', 'show');
+  });
+
+  When('A user enter the username as register',() =>{
+    cy.get('#loginusername').clear();
+    cy.get('#loginusername').type(username);
+  });
+
+  When('A user enter the login password {string}',(password) =>{
+    cy.get('#loginpassword').type(password);
+  });
+
+  When ('A user clicks in log in button', () => {
+    cy.get('[type="button"]').contains('Log in').click();
+  });
+
+  Then('The login modal should not be visible', () => {
+    cy.get('#logInModal')
+    .should('have.class', 'modal')
+    .and('have.class', 'fade')
+    .and('not.have.class', 'show');
+  });
+
+  Then('the navbar should have the name of the user', () => {
+    cy.get('#nameofuser')
+    .should('have.text', `Welcome ${username}`)
+    .invoke('text').then((linktext) =>{
+      expect(linktext).to.equal(`Welcome ${username}`);
+    });
+  });
+
+
+
+
 
